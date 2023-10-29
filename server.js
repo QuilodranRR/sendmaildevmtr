d1398051cf4ff4aad9c685d03644f136b4423cb5
QuilodranRR <128753831+QuilodranRR@users.noreply.github.com>
2023-10-29 14:29:15 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 const express = require('express');
 const app = express();
+const multer = require('multer'); // Importa la biblioteca multer.
+const cors = require('cors');
+const puerto = process.env.PORT || 3000;
 
-const puerto = process.env.PORT || 3000; // Utiliza el puerto 3000 o un puerto definido por la variable de entorno PORT.
+// Configura el almacenamiento de archivos con multer.
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // El directorio donde se guardarán los archivos.
+  },
+  filename: function (req, file, cb) {
+    // Genera un nombre de archivo único.
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 
-app.post('/subir-pdf', (req, res) => {
-    // Aquí puedes procesar el PDF, guardarlo en el servidor o realizar cualquier otra acción necesaria.
-  
-    res.status(200).json({ message: 'PDF recibido y procesado con éxito.' });
-  });
 
+app.use(cors());
 
+// Ruta para subir el archivo PDF.
+app.post('/subir-pdf', upload.single('pdfFile'), (req, res) => {
+  if (!req.file) {
+    // Si no se proporciona un archivo, devuelve un error.
+    return res.status(400).json({ error: 'No se proporcionó ningún archivo PDF.' });
+  }
 
+  // En este punto, el archivo se ha subido con éxito y se puede acceder en req.file.
+  // Puedes realizar las acciones necesarias, como guardar el archivo o procesarlo.
+
+  // Ejemplo: Guardar el archivo en una carpeta específica.
+  const filePath = req.file.path;
+
+  // A partir de aquí, puedes realizar otras acciones como enviar el archivo por correo electrónico o procesarlo de la manera que necesites.
+
+  res.status(200).json({ message: 'PDF recibido y procesado con éxito.' });
+});
 
 app.listen(puerto, () => {
   console.log(`Servidor Node.js escuchando en el puerto ${puerto}`);
 });
-
 
   
 
