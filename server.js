2fe9941712c9046962fc928e07ecc49019c11aec
QuilodranRR <128753831+QuilodranRR@users.noreply.github.com>
2023-11-10 15:25:13 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const corsOptions = {
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
